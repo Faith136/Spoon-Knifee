6fbb1804e7234416acf176a27ab71919577523f0
Faith136 <faithodongo9@gmail.com>
2025-08-11 13:08:42 +0300

    created a new branch

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-let a = 2;
+ window.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    alert('I successfully forked this repo! Happy coding! 🚀');
+  }, 600);
+});
+
+
+ let a = 2;
 console.log(a)
